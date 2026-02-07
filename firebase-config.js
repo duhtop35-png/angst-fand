6c66869597a330b3f00cf2974f928f2c9da05179
duhtop35-png <duhtop35@gmail.com>
2026-02-07 16:04:59 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAZCKP8B_n_zaZfe1vGMPFyB7FP-ap_4No",
+    authDomain: "angst-fandom.firebaseapp.com",
+    projectId: "angst-fandom",
+    storageBucket: "angst-fandom.firebasestorage.app",
+    messagingSenderId: "664399058367",
+    appId: "1:664399058367:web:609c0dda50afebd6c03d0a"
+};
