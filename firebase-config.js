1b625859af7f7535aee162612bbdf150eef34d0f
duhtop35-png <duhtop35@gmail.com>
2026-02-07 16:24:59 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// firebase-config.js
+/ firebase-config.js
 const firebaseConfig = {
     apiKey: "AIzaSyAZCKP8B_n_zaZfe1vGMPFyB7FP-ap_4No",
     authDomain: "angst-fandom.firebaseapp.com",
@@ -6,4 +6,5 @@ const firebaseConfig = {
     storageBucket: "angst-fandom.firebasestorage.app",
     messagingSenderId: "664399058367",
     appId: "1:664399058367:web:609c0dda50afebd6c03d0a"
+
 };
